5ffeda929007fd87c47b72cc1ab33feec4ef5dd9
Matheus Fidelis <msfidelis01@gmail.com>
2016-10-11 08:47:54 -0300

    Primeiro Commit

---
+++ core/classes/Crypto.php
@@ -0,0 +1,52 @@
+<?php
+namespace msfidelis\crytoUtils\classes;
+
+/**
+ *CrytoCkass
+ * @email msfidelis01@gmail.com
+ * @author Matheus Fidelis
+ */
+
+class Crypto {
+
+    protected $digest_alg = 'sha256';
+    protected $private_key_bits = '4096';
+    protected $private_key_type = OPENSSL_KEYTYPE_RSA;
+
+    public function getDigest_alg() {
+        return $this->digest_alg;
+    }
+    public function getPrivate_key_bits() {
+        return $this->private_key_bits;
+    }
+    public function getPrivate_key_type() {
+        return $this->private_key_type;
+    }
+    public function setDigest_alg($digest_alg) {
+        $this->digest_alg = $digest_alg;
+        return $this;
+    }
+    public function setPrivate_key_bits($private_key_bits) {
+        $this->private_key_bits = $private_key_bits;
+        return $this;
+    }
+    public function setPrivate_key_type($private_key_type) {
+        $this->private_key_type = $private_key_type;
+        return $this;
+    }
+    /**
+     * Gera uma chave aleatória
+     * @param type $length tamanho da chave
+     * @return type chave gerada
+     */
+    public static function randomKey($length = 12) {
+        $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&';
+        $pass = array();
+        $alphaLength = strlen($alphabet) - 1;
+        for ($i = 0; $i < $length; $i++) {
+            $n = rand(0, $alphaLength);
+            $pass[] = $alphabet[$n];
+        }
+        return implode($pass);
+    }
+}
---
+++ core/classes/EncryptSSL.php
@@ -0,0 +1,51 @@
+
+<?php
+namespace msfidelis\crytoUtils\classes;
+use msfidelis\crytoUtils\classes\Crypto;
+
+/**
+ * OpenSSL Utils
+ * @email msfidelis01@gmail.com
+ * @author Matheus Fidelis
+ */
+class EncryptSSL extends Crypto {
+
+    /**
+     * Gera um par de chaves com o OpenSSL
+     * @return type object
+     */
+    public function generataPair() {
+        $configs = array(
+            "digest_alg" => $this->digest_alg,
+            "private_key_bits" => $this->private_key_bits,
+            "private_key_type" => $this->private_key_type,
+        );
+        $res = openssl_pkey_new($configs);
+        $pubKey = openssl_pkey_get_details($res)["key"];
+        $privKey = '';
+        openssl_pkey_export($res, $privKey);
+        return (object) array('pubkey' => $pubKey, 'privkey' => $privKey);
+    }
+
+    /**
+     * Encrypt with Public Key
+     * @param type $plain texto limpo
+     * @param type $PubKey Chave pública do usuário
+     */
+    public static function encrypt($plain, $PubKey) {
+        $encrypted = null;
+        openssl_public_encrypt($plain, $encrypted, $PubKey);
+        return $encrypted;
+    }
+
+    /**
+     * Decrypt with Private Key
+     * @param type $enc texto encryptado
+     * @param type $PrivKey Chave privada do usuário
+     */
+    public static function decrypt($enc, $PrivKey) {
+        $decrypted = null;
+        openssl_private_decrypt($enc, $decrypted, $PrivKey);
+        return $decrypted;
+    }
+}
